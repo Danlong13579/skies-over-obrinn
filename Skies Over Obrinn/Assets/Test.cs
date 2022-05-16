bd95cc15c10c2e5e0148bfe8cabe0ac6421ab1d9
Daniel Long <Danlong13579@gmail.com>
2022-05-16 02:44:38 -0400

    Rider IDE Test

---
+++ Skies Over Obrinn/Assets/Test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.UI;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(rd);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
